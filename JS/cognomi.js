441ec8dcbc5d2594ea5181879f1737b273619d82
Carmine Lentisco <carminelentisco@gmail.com>
2020-03-31 15:51:25 +0200

    JS
    Ordine alfabetico + stampa

--- JS/cognomi.js
+++ JS/cognomi.js
@@ -6,7 +6,12 @@ var cognomeUente = prompt('Inserisci il tuo cognome');
 var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 
 // Aggiunta 
+
 listaCognomi.push(cognomeUente);
+console.log('Lista cognomi in ordine sparso : ', listaCognomi )
 
 // Stampa la lista in ordine alfabetico
+
+listaCognomi.sort();
+console.log('Lista cognomi ordinata alfabeticamente : ', listaCognomi);
 
