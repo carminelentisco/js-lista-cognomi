f76fb554cdc161f15f3a3a812ea9a311157cfa27
Carmine Lentisco <carminelentisco@gmail.com>
2020-03-31 15:16:59 +0200

    JS aggiunta del cognome

--- JS/cognomi.js
+++ JS/cognomi.js
@@ -2,3 +2,11 @@
 
 //console.log('cognomi.js è stato inserito correttamente.');
 
+var cognomeUente = prompt('Inserisci il tuo cognome');
+var listaCognomi = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+
+// Aggiunta 
+listaCognomi.push(cognomeUente);
+
+// Stampa la lista in ordine alfabetico
+
